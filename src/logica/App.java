fb8c9eb6cac8bbb3dfd479e46e110ae342918394
diegoserrano-cloud <diego.serrano@alumnos.ucn.cl>
2026-05-17 12:39:53 -0400

    first commit

---
+++ src/logica/App.java
@@ -0,0 +1,16 @@
+/*
+ * Nombres: Diego Serrano Fuentes, Josselyn Barraza Yáñez
+ * Rut(s): 22.105.561-6 | 22.246.539-7
+ * Carrera: ICCI
+ * Taller: Taller N° 3 POO
+ */
+
+
+package logica;
+
+public class App {
+	public static void main(String[] args) {
+	
+		System.out.println("hola taller 3");
+	}
+}
